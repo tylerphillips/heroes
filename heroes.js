3690b92fc6d70eab6f259bd6a97a8309ac58e6a3
Tyler Phillips <tylerphillips@Tylers-MacBook-Air.local>
2013-08-26 23:23:03 -0400

    Upload js and html

---
+++ heroes.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+
+	heroes=[
+				{
+					hero: "The Neon Lad",
+						realName: "Mason Stewart",
+						power: "Laser Blasts",
+						how: "Freak nuclear accident",
+						rival: "Butcher Brisbane",
+						affiliation: "Villain",
+						group: "Alchemy Guard",
+					
+				},
+				{
+					hero: "Doctor Phenomenal",
+						realname: "Eric Dodds",
+						power: "Super Sight",
+						how: "Side-effect of computer chip implant to restore his vision",
+						rival: "Mustapha Jones",
+						affiliation: "Hero",
+						group: "Fantastic Battalion"
+				},
+				{
+					hero: "Wolf Cat",
+						realname: "Ryan Ponce",
+						power: "Lycanthropy: transforms into werewolf at will",
+						how: "Raised by hyper-intelligent wolves",
+						rival: "Cold Henry",
+						affiliation: "Villain",
+						group: "Mystifying Rejects"
+				},
+				{
+					hero: "Duke Puma",
+						realName: "Andy Flack",
+						power: "Cat Communication",
+						how: "Bitten by radioactive cat",
+						rival: "Adam and the Adamantines",
+						affiliation: "Hero",
+						group: "Alpha Inc",
+				},
+				{
+					hero: "The Hyper Master",
+						realName: "Ryan Poplin",
+						power: "Super Speed",
+						how: "Experiment gone wrong",
+						rival: "The Man from Elsewhere",
+						affiliation: "Villain",
+						group: "Alchemy Guard",
+				},
+				{
+					hero: "Wonder Flame",
+						realName: "Joe Tamburro",
+						power: "Indestructability and Pyrokensis",
+						how:"Ancient Olympian god sent to Earth to teach people a lesson in humanity",
+						rival:"Daniel 'Soul Stealer' Malone",
+						affiliation:"Hero",
+						group:"Fantastic Battalion",
+				},
+				{
+					hero: "The Mighty Wind",
+						realName: "Joe VanLeeuwen",
+						power: "Persuasion",
+						how:"Cracked a mathematical code that unlocked his powers",
+						rival:"Mad Man Martin",
+						affiliation:"Villain",
+						group:"Mystifying Rejects",
+				},
+				{
+					hero: "Duke Suave",
+						realName: "Tyler Phillips",
+						power: "Seduction",
+						how:"Trained overseas with the ancient Shaolin Masters",
+						rival:"Benny Da Vinci",
+						affiliation:"Hero",
+						group:"Alpha Inc",
+				},
+				{
+					hero: "Mr Neutron",
+						realName: "Brandon Miller",
+						power: "Absorbing Kinetic Energy",
+						how:"Freak nuclear accident",
+						rival:"The Man from Elsewhere",
+						affiliation:"Villain",
+						group:"Alchemy Guard",
+				},
+				{
+					hero: "The Skeleton Lad",
+						realName: "Areyeh Picker",
+						power: "Summons skeletal warriors",
+						how:"Gifted with a mystical scroll",
+						rival:"Gregory Deathstar",
+						affiliation:"Hero",
+						group:"Fantastic Battalion",
+				},
+				{
+					hero: "The Awesome Braid",
+						realName: "Allison Miller",
+						power: "Prepensile Hair",
+						how:"Knocked into a vat of poisoness chemicals by Admiral Beefheart",
+						rival:"Admiral Beefheart",
+						affiliation:"Villain",
+						group:"Mystifying Rejects",
+				},
+				{
+					hero: "Neptune Mask",
+						realName: "Jake Hendley",
+						power: "Can summon the forces of any planet or moon",
+						how:"Bequeathed powers by a dying alien who identified him as the bravest person on Earth",
+						rival:"The Ellipse",
+						affiliation:"Hero",
+						group:"Alpha Inc",
+				},
+				{
+					hero: "Hyper Hex",
+						realName: "Keeron Thandroyen",
+						power: "Super Speed and can summon skeletal warriors",
+						how:"Experiment gone wrong",
+						rival:"Lee Pin Woo, Master of Mysticism",
+						affiliation:"Villain",
+						group:"Alchemy Guard",
+				},
+				{
+					hero: "Star Flame",
+						realName: "Caleb Bradley",
+						power: "Can create miniature, super-heated stars that can be used as weapons and Pyrokensis",
+						how:"An alien brought to Earth and trapped by accident trying to find his way home",
+						rival:"Il Zuko",
+						affiliation:"Hero",
+						group:"Fantastic Battalion",
+				},
+				{
+					hero: "Hornet Mask",
+						realName: "Daniel Jeffords",
+						power: "Poisoness sting that can disable or kill with deadly speed",
+						how:"Stung by a radioactive insect",
+						rival:"Anne of Crimson Gables",
+						affiliation:"Villain",
+						group:"Mystifying Rejects",
+				},
+
+			]
+	heroes.forEach (function(hero) {
+	
+		})	
+});
+
+
+
+
